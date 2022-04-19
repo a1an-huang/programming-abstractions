c4252064c87a1bd6911e4ac9c7dd7286d880a930
Alan Huang <98438095+a1an-huang@users.noreply.github.com>
2022-04-18 23:37:40 -0400

    Add files via upload

---
+++ BijectionGroup.java
@@ -0,0 +1,20 @@
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class BijectionGroup {
+
+    public static T bijectionOf(Set<T> domain) {
+
+    }
+
+    public static void main(String... args) {
+        Set<Integer> a_few = Stream.of(1, 2, 3).collect(Collectors.toSet());
+        // you have to figure out the data type in the line below
+        BijectionGroup bijections = bijectionsOf(a_few);
+        bijections.forEach(aBijection -> {
+            a_few.forEach(n -> System.out.printf("%d --> %d; ", n, aBijection.apply(n)));
+            System.out.println();
+        });
+    }
+}
